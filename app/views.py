f374b4d1fdc8c249fd2aad18c85c6eb7450fafe4
Whoislead <94552006+Whoislead@users.noreply.github.com>
2024-04-01 12:25:24 -0500

    added my app folder

---
+++ app/views.py
@@ -0,0 +1,74 @@
+from app import app
+from flask import render_template, request, redirect, url_for, flash
+
+
+from flask_mail import Message
+from app.forms import ContactForm
+from app import app, mail
+
+
+###
+# Routing for your application.
+###
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data, sender=(form.name.data,form.email.data),recipients=["34d6cb00c7-007b8a+1@inbox.mailtrap.io"])
+        msg.body = form.message_area.data
+        mail.send(msg)
+        flash("Message successfully sent!")
+        return redirect(url_for("home"))
+    return render_template('contact.html', form=form)
+
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+
+# Flash errors from the form if validation fails
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
+
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also tell the browser not to cache the rendered page. If we wanted
+    to we could change max-age to 600 seconds which would be 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
